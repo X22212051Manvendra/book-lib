f262feac56193196c0682f23136620dbf0bf8528
X22212051Manvendra <x22212051@student.ncirl.ie>
2024-04-18 11:15:58 +0530

    project complete

--- web/src/lib/appconstants.js
+++ web/src/lib/appconstants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "http://34.253.108.176:8000";
 
 export const CLASSMATE_API =
   "https://vf0mar3ewj.execute-api.us-east-1.amazonaws.com/quotes";
